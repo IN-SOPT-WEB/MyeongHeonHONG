add1dc8d704d596ff5d1efe00568a7cce7a62f1f
myeongheonhong <ruddyh7076@gmail.com>
2022-11-04 01:44:41 +0900

    [ Feat ]  : Main Component 기능구현 - image를 보여주는 IamgeBox 구현, - 이름 보기를 보여주는 NameList&NameItem 구현, - NameItem에는 onClick 속성을 통해 props로 전달받은 checkPerson에 클릭 된 보기에 해당되는 이름을 인자로 전달. - ResetBtn을 클릭 시 resetGame함수를 실행시킴.

--- week03/src/components/Main.jsx
+++ week03/src/components/Main.jsx
@@ -1,5 +1,79 @@
-export default function Main() {
-  return(
-    <div>안녕 나는 메인이야!</div>
-  );
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const StyledMain = styled.main`
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ImageBox = styled.img`
+    width: 400px;
+    height: 400px;
+    border-radius: 5px;
+    object-fit: cover;
+`;
+
+const NameList = styled.div`
+    list-style: none;
+    width: 400px;
+    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+`;
+
+const NameItem = styled.button`
+    list-style: none;
+    width: 75px;
+    height: 35px;
+`;
+
+const ResetBtn = styled.button`
+    list-style: none;
+    width: 210px;
+    height: 35px;
+`;
+
+const BtnWrap = styled.div`
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+`;
+
+export default function Main(props) {
+    const { image, example, checkPerson, resetGame, index, length } = props;
+
+    return (
+        <StyledMain>
+            <ImageBox src={image}></ImageBox>
+            {index !== length && (
+                <NameList>
+                    <NameItem onClick={() => checkPerson(example[0])}>
+                        {example[0]}
+                    </NameItem>
+                    <NameItem onClick={() => checkPerson(example[1])}>
+                        {example[1]}
+                    </NameItem>
+                    <NameItem onClick={() => checkPerson(example[2])}>
+                        {example[2]}
+                    </NameItem>
+                    <NameItem onClick={() => checkPerson(example[3])}>
+                        {example[3]}
+                    </NameItem>
+                    <NameItem onClick={() => checkPerson(example[4])}>
+                        {example[4]}
+                    </NameItem>
+                </NameList>
+            )}
+            <BtnWrap>
+                <ResetBtn onClick={resetGame}>다시하기</ResetBtn>
+            </BtnWrap>
+        </StyledMain>
+    );
 }
