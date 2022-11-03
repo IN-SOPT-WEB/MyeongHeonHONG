3185fc29caa93da594f04a21210bc65a0bf9cdde
myeongheonhong <ruddyh7076@gmail.com>
2022-11-04 01:58:25 +0900

    [ Feat ] : "App 컴포넌트 기능구현"
    - [정답,이미지,5명의 보기]의 배열정보를 담고있는 people을 선언.
    - Main에서 전달된 checkPerson의 매개변수가 정답과 일치한다면 answer의 상태를 true로 변경.
      useEffect에서 answer의 상태가 변경시 Point와 다음문제(Index증가)로 넘어감.
    - resetGame이 호출될 경우 reset의 값을 true로 변경 시 useEffect에서 점수와 문제(Index)를 초기화.
    - 조건에 따라 전달하는 props를 다르게하여 마지막 문제를 맞출 시 끝나는 메세지를 보여주는 화면 구현.

--- week03/src/App.jsx
+++ week03/src/App.jsx
@@ -1,17 +1,97 @@
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
-import Footer from './components/Footer';
 
-function App() {
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import image1 from './image/공유.jpeg';
+import image2 from './image/마동석.jpeg';
+import image3 from './image/서프라이즈.jpeg';
+import image4 from './image/손석구.jpg';
+import image5 from './image/윤지영.jpeg';
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #ecc9c9;
+`;
+
+const people = [
+    ['공유', image1, ['홍명헌', '정현욱', '이주함', '공유', '김형겸']],
+    ['마동석', image2, ['박현지', '장명지', '이서영', '마동석', '윤지영']],
+    ['이수완', image3, ['이수완', '이수안', '서프라이즈 걔', '이수온', '이수']],
+    ['손석구', image4, ['김필', '홍명헌', '손석구', '발석구', '티벳여우']],
+];
+
+const Done = styled.div`
+    display: flex;
+    justify-content: center;
+    height: 40px;
+    font-size: 32px;
+    font-weight: 700;
+`;
+
+export default function App() {
+    const [index, setIndex] = useState(0);
+    const [point, setPoint] = useState(0);
+    const [answer, setAnswer] = useState(false);
+    const [reset, setReset] = useState();
+
+    const checkPerson = (example) => {
+        if (example === people[index][0]) {
+            alert('정답!!');
+            setAnswer(true);
+        } else {
+            alert('땡!!');
+            setAnswer(false);
+        }
+    };
+
+    const resetGame = () => {
+        setReset(true);
+    };
+
+    useEffect(() => {
+        if (reset === true) {
+            setIndex(0);
+            setPoint(0);
+        }
+        setReset(false);
+    }, [reset]);
+
+    useEffect(() => {
+        if (answer) {
+            setPoint(point + 1);
+            setIndex(index + 1);
+        }
+        setAnswer(false);
+    }, [answer]);
+
     return (
-        <div className="App">
+        <Container>
             <Header />
-            <Nav count="1" />
-            <Main/>
-            <Footer/>
-        </div>
+            <Nav point={point} />
+            {index === people.length && <Done>!끝!</Done>}
+            {index < people.length && (
+                <Main
+                    image={people[index][1]}
+                    example={people[index][2]}
+                    person={people[index][0]}
+                    checkPerson={checkPerson}
+                    resetGame={resetGame}
+                    index={index}
+                    lenght={people.length}
+                />
+            )}
+            {index === people.length && (
+                <Main
+                    image={image5}
+                    example={people[index - 1][2]}
+                    person={people[index - 1][0]}
+                    checkPerson={checkPerson}
+                    resetGame={resetGame}
+                />
+            )}
+        </Container>
     );
 }
-
-export default App;
