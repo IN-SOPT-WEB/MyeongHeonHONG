db9c98838976eeda650243015b21b831cf8a681c
myeongheonhong <ruddyh7076@gmail.com>
2022-11-04 01:46:09 +0900

    [ Fix ] : Styled Component 적용

--- week03/src/components/Nav.jsx
+++ week03/src/components/Nav.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const StyleddNav = styled.nav`
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    font-weight: 700;
+`;
+
+Nav.defaultProps = {
+    point: '0',
+};
+
 export default function Nav(props) {
-  return (
-    <div>{props.count} 점</div>
-  )
+    return <StyleddNav>{props.point} 점</StyleddNav>;
 }
