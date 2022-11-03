f2ed87c6d0978334a7b66c0edc0a5494385bd808
myeongheonhong <ruddyh7076@gmail.com>
2022-11-03 19:00:27 +0900

    Fix : header영역 디자인 수정 과제 틀을 잡기위해 설정한 값 변경.

--- week03/src/components/Header.jsx
+++ week03/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 32px;
+    font-weight: 700;
+    color: white;
+`;
+
 export default function Header() {
-    return (
-        <div>
-            <h1>당신 누군교~~!!</h1>
-        </div>
-    );
+    return <StyledHeader>👩🏻‍🦰👩🏻‍🦳인물퀴즈 게임👨🏻‍🦳👨🏻‍🦰</StyledHeader>;
 }
