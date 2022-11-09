7e90e34ca44cdf120e00fa79bb0582ebcaddce32
myeongheonhong <ruddyh7076@gmail.com>
2022-11-10 03:17:19 +0900

    [ Feat ] : props를 통해 받아온 githubData를 통해 원하는 정보를 출력하는 컴포넌트 구현.

--- week04/src/App.jsx
+++ week04/src/App.jsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import './App.css';
 import Finder from './components/Finder';
+import Content from './components/Content';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const API_URL = 'https://api.github.com/users/';
 
 export default function App() {
     const [githubData, setGithubData] = useState();
-    const [githubUserList, setGithubUserList] = useState(new Set());
+    const [githubSearchList, setGithubSearchList] = useState([]);
+    const [openContent, setOpenContent] = useState(false);
 
     const bringUsername = (username) => {
         axios
@@ -15,21 +18,24 @@ export default function App() {
             .then((resopnse) => {
                 if (resopnse['status'] === 200) {
                     setGithubData(resopnse['data']);
-                    setGithubUserList((prev) => new Set(prev.add(username)));
+                    setGithubSearchList((prev) => [
+                        ...new Set(prev).add(username),
+                    ]);
+                    setOpenContent(true);
                 }
             })
             .catch((error) => {
                 alert(error);
             });
     };
-    console.log(githubUserList);
 
     return (
         <Container>
             <Finder
                 bringUsername={bringUsername}
-                githubUserList={githubUserList}
+                githubSearchList={githubSearchList}
             ></Finder>
+            {openContent && <Content githubData={githubData}></Content>}
         </Container>
     );
 }
--- week04/src/components/Content/index.jsx
+++ week04/src/components/Content/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+export default function Index(props) {
+    const { githubData } = props;
+    console.log(githubData);
+    return (
+        <ContentContainer>
+            <div>{githubData['followers']}</div>
+            <div>{githubData['following']}</div>
+            <a href={githubData['html_url']}>깃허브 주소방문</a>
+            <img src={githubData['avatar_url']} alt="" />
+            <div>{githubData['login']}</div>
+            <div>{githubData['name']}</div>
+        </ContentContainer>
+    );
 }
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    width: 550px;
+    height: 400px;
+    border-radius: 10px;
+    background-color: #6e5494;
+`;
