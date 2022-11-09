4fb217d9b02e7b87d5bb837232444f21e2d9a345
myeongheonhong <ruddyh7076@gmail.com>
2022-11-09 14:59:46 +0900

    [ Feat ] : Finder 컴포넌트 기본구성

--- week04/src/components/Finder/index.jsx
+++ week04/src/components/Finder/index.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import Title from './Title';
+import styled from 'styled-components';
 
 export default function index() {
     return (
-        <div>
-            <Title title="GitHub Profile Finder!!"></Title>
-        </div>
+        <FinderContainer>
+            <Title>GitHub Profile Finder</Title>
+            <Input type="text" placeholder="GitHub Username..."></Input>
+        </FinderContainer>
     );
 }
+
+const FinderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    width: 550px;
+    height: 100px;
+    margin: 40px;
+    border-radius: 10px;
+    background-color: #6e5494;
+`;
+
+const Title = styled.h1`
+    color: white;
+    font-size: 32px;
+    font-weight: 700;
+`;
+
+const Input = styled.input`
+    width: 200px;
+    height: 30px;
+    border-radius: 7px;
+    border: none;
+    text-indent: 15px;
+`;
